c7d44c08671ae976c375cb6fcce71943b98777b9
Maxime Rousseau <maximerousseau08@gmail.com>
2024-07-24 16:14:14 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="rafale",
+    version="0.1",
+    description="simple transformer training lib",
+    url="https://github.com/maxrousseau/rafale",
+    author="maxime rousseau",
+    packages=["rafale"],
+    zip_safe=False,
+)
